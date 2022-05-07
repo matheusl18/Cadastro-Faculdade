be060fd16347e6a73b093ba5900288eee0d1be42
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-05-07 01:56:59 -0300

    Create Teste.java

---
+++ Teste.java
@@ -0,0 +1,37 @@
+package aps;
+
+import java.util.List;
+
+public class Teste {
+public static void main(String[] args) {
+		
+		List<Aluno> alunos = Arquivos.loadAlunos("C:\\Facul\\Aps Java/Alunos.csv");
+		for(Aluno aluno: alunos){
+            System.out.println(aluno);
+		}
+		
+		Aluno aluno1 = new Aluno("a1", "Gabriele" );
+		alunos.add(aluno1);
+		for(Aluno aluno: alunos){
+            System.out.println(aluno);
+        }
+		Arquivos.saveAlunos(alunos, "C:\\Facul\\Aps Java/Alunos.csv");
+
+	
+
+	
+	List<Curso> cursos = Arquivos.loadCursos("C:\\Facul\\Aps Java/Curso.csv");
+	for(Curso curso: cursos){
+        System.out.println(curso);
+	}
+	
+	Curso curso1 = new Curso("Ciencias da Computação", "Graduação", 2021 );
+	cursos.add(curso1);
+	for(Curso curso: cursos){
+        System.out.println(curso);
+    }
+	Arquivos.saveCursos(cursos, "C:\\Facul\\Aps Java/Curso.csv");
+
+}
+
+}
