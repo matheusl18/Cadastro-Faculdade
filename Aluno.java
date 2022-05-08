1c339e2b93861b486c613f30e4aaef200700f875
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-05-08 18:17:38 -0300

    Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -16,8 +16,14 @@ public class Aluno implements Comparable<Aluno>{
 	public String getId() {
 		return this.id;
 	}
+        public void setId(String id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return this.nome;
+	}
+        public void setNome(String nome) {
+		this.nome = nome;
 	}
 	
 	
