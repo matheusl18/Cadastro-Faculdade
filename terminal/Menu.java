7fd804bd2e5ba5a0a25a0a3ec8bfd5d5d9b87e02
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-05-13 21:55:53 -0300

    Menu.java

--- terminal/Menu.java
+++ terminal/Menu.java
@@ -1,18 +1,20 @@
-package terminal;
+package Interface;
 
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 import java.util.TreeMap;
 
-import aps.Arquivos;
-import aps.Aluno;
-import aps.Curso;
+import Data.Arquivos;
+import Entidades.Aluno;
+import Entidades.Curso;
+import Entidades.Registro;
 
 
 
 public class Menu {
-	private TratamentoErros terminal;
+	private Visualizacao terminal;
+	private Arquivos data;
+	private Registro registro;
 	
 	
 	public static Map<Integer, Opcao> opc = new TreeMap<>();
@@ -21,15 +23,15 @@ public class Menu {
 			opc.put(o.codigo, o);
 		}
 	}
-	public Menu(TratamentoErros terminal) {
+	public Menu(Visualizacao terminal, Arquivos data) {
 		this.terminal = terminal;
+		this.data = data;
 		
 	}
 	public void init(){
+		registro = data.getRegistro();
 		int x;
 		Scanner scan = new Scanner(System.in);
-		Set<Aluno> alunos = Arquivos.loadAlunos("C:\\Facul\\Aps Java/Alunos.csv");
-		Set<Curso> cursos = Arquivos.loadCursos("C:\\Facul\\Aps Java/Curso.csv");
 		Opcao codigo = null;
 		while(codigo != Opcao.SAIR) {
 			codigo = terminal.menu();
@@ -37,11 +39,7 @@ public class Menu {
 			switch(codigo) {
 			case LISTA_ALUNOS:
 				while(x == 1) {
-					System.out.println("Lista de Alunos");
-					
-					for(Aluno aluno: alunos){
-			            System.out.println(aluno);
-					}
+					listarAlunos();
 					System.out.println("\n\n1 Continuar\n2 voltar");
 					x = scan.nextInt();
 				}
@@ -49,84 +47,54 @@ public class Menu {
 				break;
 			case LISTA_CURSOS:
 				while(x == 1) {
-					System.out.println("Lista de Cursos");
-					for(Curso curso: cursos){
-				        System.out.println(curso);
-					}
+					listarCursos();
 					System.out.println("\n\n1 Continuar\n2 voltar");
 					x = scan.nextInt();
 				}
 	
 				break;
 			case LISTA_ALUNOS_DE_CURSO:
+				listaAlunosPorCurso();
 				break;
 			case LISTA_CURSOS_DE_ALUNO:
+				listaCursosPorAluno();
 				break;
 			case ADICIONA_ALUNO:
 				while(x == 1) {
-					Scanner input = new Scanner(System.in);
-					System.out.println("Digite a Id do aluno: ");
-					String id = input.nextLine();
-					System.out.println("Digite o nome do aluno: ");
-					String nome = input.nextLine();
-					Aluno aluno1 = new Aluno(id, nome );
-					alunos.add(aluno1);
-					Arquivos.saveAlunos(alunos, "C:\\Facul\\Aps Java/Alunos.csv");
+					adicionarAluno();
 					System.out.println("\n\n1 Continuar\n2 voltar");
 					x = scan.nextInt();
 				}
 				break;
 			case ADICIONA_CURSO:
 				while(x == 1) {
-					Scanner input2 = new Scanner(System.in);
-					System.out.println("Digite o nome do curso: ");
-					String nome2 = input2.nextLine();
-					System.out.println("Digite o nivel do curso: ");
-					String nivel = input2.nextLine();
-					System.out.println("Digite o ano do curso: ");
-					int ano = input2.nextInt();
-					Curso curso1 = new Curso(nome2, nivel, ano );
-					cursos.add(curso1);
-					Arquivos.saveCursos(cursos, "C:\\Facul\\Aps Java/Curso.csv");
+					adicionarCurso();
 					System.out.println("\n\n1 Continuar\n2 voltar");
 					x = scan.nextInt();
 				}
 				break;
 			case ADICIONA_RELACAO:
+				adicionarRelacao();
 				break;
 			case REMOVER_ALUNO:
 				while(x == 1) {
-					Scanner input3 = new Scanner(System.in);
-					System.out.println("Digite a Id do aluno: ");
-					String id2 = input3.nextLine();
-					System.out.println("Digite o nome do aluno: ");
-					String nome3 = input3.nextLine();
-					Aluno aluno2 = new Aluno(id2, nome3 );
-					alunos.remove(aluno2);
-					Arquivos.saveAlunos(alunos, "C:\\Facul\\Aps Java/Alunos.csv");
+					removerAluno();
 					System.out.println("\n\n1 Continuar\n2 voltar");
 					x = scan.nextInt();
 				}
 				break;
 			case REMOVER_CURSO:
 				while(x == 1) {
-					Scanner input4 = new Scanner(System.in);
-					System.out.println("Digite o nome do curso: ");
-					String nome4 = input4.nextLine();
-					System.out.println("Digite o nivel do curso: ");
-					String nivel2 = input4.nextLine();
-					System.out.println("Digite o ano do curso: ");
-					int ano2 = input4.nextInt();
-					Curso curso2 = new Curso(nome4, nivel2, ano2 );
-					cursos.remove(curso2);
-					Arquivos.saveCursos(cursos, "C:\\Facul\\Aps Java/Curso.csv");
+					removerCurso();
 					System.out.println("\n\n1 Continuar\n2 voltar");
 					x = scan.nextInt();
 				}
 				break;
 			case REMOVER_RELACAO:
+				removerRelacao();
 				break;
 			case SAIR:
+				terminar();
 				break;
 			default:
 				throw new IllegalArgumentException("Valor Desconhecido: " + opc);
@@ -136,5 +104,63 @@ public class Menu {
 			
 		}
 
+	}
+	private void listarAlunos() {
+		terminal.listarAlunos(registro);
+	}
+	
+	private void listarCursos() {
+		terminal.listarCursos(registro);
+	}
+	
+	private void adicionarAluno() {
+		Aluno aluno = terminal.addAluno();
+		this.registro.addAluno(aluno);
+	}
+	private void removerAluno() {
+		Aluno aluno = terminal.delAluno();
+		this.registro.delAluno(aluno);
+	}
+	
+	private void adicionarCurso() {
+		Curso curso = terminal.addCurso();
+		this.registro.addCurso(curso);
+	}
+	private void removerCurso() {
+		Curso curso = terminal.delCurso();
+		this.registro.delCurso(curso);
+	}
+	
+	private void listaAlunosPorCurso() {
+		Curso curso = terminal.getCursoPorLista(registro);
+		if(curso==null) return;
+		terminal.listarAlunosPorCurso(registro, curso);
+	}
+	
+	
+	private void listaCursosPorAluno() {
+		Aluno aluno = terminal.getAlunoPorLista(registro);
+		if(aluno==null) return;
+		terminal.listarCursosPorAluno(registro, aluno);
+	}
+	
+	private void adicionarRelacao() {
+		Aluno aluno = terminal.getAlunoPorLista(registro);
+		if(aluno==null) return;
+		Curso curso = terminal.getCursoPorLista(registro);
+		if(curso==null) return;
+		registro.addRelacaoAlunoCurso(aluno, curso);
+	}
+	private void removerRelacao() {
+		Aluno aluno = terminal.getAlunoPorLista(registro);
+		if(aluno==null) return;
+		Curso curso = terminal.getCursoPorLista(registro);
+		if(curso==null) return;
+		registro.delRelacaoAlunoCurso(aluno, curso);
+		
+	}
+	
+	private void terminar() {
+	data.saveRegistro(registro);
 	}
 }
