d7d5de5748be96936748826e8a526f9611316e40
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-05-13 21:58:13 -0300

    Update Arquivos.java

--- Arquivos.java
+++ Arquivos.java
@@ -1,4 +1,6 @@
-package aps;
+package Data;
+
+
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -11,16 +13,89 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
-import java.util.TreeSet;
-
+import Entidades.Aluno;
+import Entidades.Curso;
+import Entidades.Registro;
 
 public class Arquivos {
-	public static Set<Aluno> loadAlunos(String filePath){
+	private String alunoPath;
+	private String cursoPath;
+	private String relacaoPath;
+	
+	private Registro registroInput;
+	
+	public Arquivos(String aAlunoPath, String aCursoPath, String aRelacaoPath) {
+		this.alunoPath = aAlunoPath;
+		this.cursoPath = aCursoPath;
+		this.relacaoPath = aRelacaoPath;
+	}
+	
+	public Registro getRegistro() {
+		this.registroInput = new Registro();
+		
+		loadAlunos();
+		loadCursos();
+		return loadRelacoes();
+	}
+	
+	private Registro loadRelacoes(){
+		
+        try (   InputStream is = new FileInputStream(this.relacaoPath);
+                InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                BufferedReader br = new BufferedReader(isr);
+            ){
+            String linha;
+            int i=0;
+            while((linha = br.readLine()) != null){
 
+                String[] palavras = linha.split(",");
+
+                String idAluno = palavras[0];
+                String nomeCurso = palavras[1];
+                String nivelCurso = palavras[2];
+                int anoCurso = Integer.parseInt(palavras[3]);
+
+                Curso curso = new Curso(nomeCurso, nivelCurso, anoCurso);
+                Aluno aluno = this.registroInput.getAlunosPorId(idAluno);
+                
+                registroInput.addRelacaoAlunoCurso(aluno, curso);
+
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         
-        Set<Aluno> alunos = new TreeSet<>();
+        return this.registroInput; 
 
-        try (   InputStream is = new FileInputStream(filePath);
+    }
+	public void saveRegistro(Registro registroOutput) {
+		saveAlunos(registroOutput.getAlunos());
+		saveCursos(registroOutput.getCursos());
+		saveRelacoes(registroOutput);
+	}
+	
+	private void saveRelacoes(Registro registroOutput){
+	
+	    try(    OutputStream os = new FileOutputStream(this.relacaoPath);
+	            OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+	            PrintWriter pw = new PrintWriter(osw, true);
+	            ){
+	        for(Aluno aluno: registroOutput.getAlunos()){
+	            for(Curso curso: registroOutput.getCursosPorAluno(aluno.getId())){
+	                pw.println(aluno.getId()+","+curso.getNome()+","+curso.getNivel()+","+curso.getAno());
+	            }
+	        }
+	
+	    }catch(IOException e){
+	        e.printStackTrace();
+	    }
+	
+	}
+	
+	private void loadAlunos(){
+
+        try (   InputStream is = new FileInputStream(this.alunoPath);
                 InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
                 BufferedReader br = new BufferedReader(isr);
             ){
@@ -41,7 +116,7 @@ public class Arquivos {
                 
 
                 Aluno aluno = new Aluno(id, nome);
-        		alunos.add(aluno);
+        		this.registroInput.addAluno(aluno);
 
             }
 
@@ -49,17 +124,16 @@ public class Arquivos {
             e.printStackTrace();
         }
 
-        return alunos;
 
     }
 
-    public static void saveAlunos(Set<Aluno> alunos, String filePath){
+    private void saveAlunos(Set<Aluno> alunosOutput){
 
-        try(    OutputStream os = new FileOutputStream(filePath/*, true*/);
+        try(    OutputStream os = new FileOutputStream(this.alunoPath);
                 OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
                 PrintWriter pw = new PrintWriter(osw, true);
                 ){
-            for(Aluno aluno: alunos){
+            for(Aluno aluno: alunosOutput){
                 pw.println(aluno.getId()+","+aluno.getNome());
             }
 
@@ -68,12 +142,9 @@ public class Arquivos {
         }
 
     }
-    public static Set<Curso> loadCursos(String filePath){
-    	
-        
-        Set<Curso> cursos = new TreeSet<>();
+    private void loadCursos(){
 
-        try (   InputStream is = new FileInputStream(filePath);
+        try (   InputStream is = new FileInputStream(this.cursoPath);
                 InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
                 BufferedReader br = new BufferedReader(isr);
             ){
@@ -95,7 +166,7 @@ public class Arquivos {
                 
 
                 Curso curso = new Curso(nome, nivel, ano);
-                cursos.add(curso);
+                this.registroInput.addCurso(curso);
 
             }
 
@@ -103,17 +174,15 @@ public class Arquivos {
             e.printStackTrace();
         }
 
-        return cursos;
-
     }
 
-    public static void saveCursos(Set<Curso> cursos, String filePath){
+    private void saveCursos(Set<Curso> cursosOutput){
 
-        try(    OutputStream os = new FileOutputStream(filePath/*, true*/);
+        try(    OutputStream os = new FileOutputStream(this.cursoPath);
                 OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
                 PrintWriter pw = new PrintWriter(osw, true);
                 ){
-            for(Curso curso: cursos){
+            for(Curso curso: cursosOutput){
                 pw.println(curso.getNome()+","+curso.getNivel()+","+curso.getAno());
             }
 
